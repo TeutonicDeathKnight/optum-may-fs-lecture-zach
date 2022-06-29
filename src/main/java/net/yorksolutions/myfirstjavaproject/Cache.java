ffe5f9d59ce75b2818248db97e53abd7e90e89b2
Zachary Seeley <zseeley7913@gmail.com>
2022-06-29 09:08:30 -0500

    auto gen id's

--- src/main/java/net/yorksolutions/myfirstjavaproject/Cache.java
+++ src/main/java/net/yorksolutions/myfirstjavaproject/Cache.java
@@ -1,12 +1,15 @@
 package net.yorksolutions.myfirstjavaproject;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 // @Entity is an annotation to tell Spring this class is a table
 @Entity
 public class Cache {
 	@Id
+			@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	Long id;
 
 	Integer input;
